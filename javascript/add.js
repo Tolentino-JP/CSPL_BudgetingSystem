838bbfe3158cb175b444f30303ae87c010ecc17b
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-04-24 23:34:37 +0800

    need js script and need database

---
+++ javascript/add.js
@@ -0,0 +1,10 @@
+let addAllowance = document.getElementById("add-allowance");
+let addExpenses = document.getElementById("add-expenses");
+const submitAllowance = document.getElementsByClassName("submit-allowance");
+const submitExpenses = document.getElementsByClassName("submit-expenses");
+const allowanceAmount = document.getElementsByClassName("allowance-amount");
+const expensesAmount = document.getElementsByClassName( "expenses-amount" );
+const balanceAmount = document.getElementsByClassName("balance-amount");
+const list = document.getElementsByClassName("list");
+
+
