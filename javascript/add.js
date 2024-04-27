24292638bf1f421b3ed76812548c8c7fc6311cb8
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-04-27 23:32:18 +0800

    updated as of 04/27/2024

--- javascript/add.js
+++ javascript/add.js
@@ -1,10 +1,52 @@
 let addAllowance = document.getElementById("add-allowance");
 let addExpenses = document.getElementById("add-expenses");
-const submitAllowance = document.getElementsByClassName("submit-allowance");
-const submitExpenses = document.getElementsByClassName("submit-expenses");
-const allowanceAmount = document.getElementsByClassName("allowance-amount");
-const expensesAmount = document.getElementsByClassName( "expenses-amount" );
-const balanceAmount = document.getElementsByClassName("balance-amount");
-const list = document.getElementsByClassName("list");
+const submitAllowance = document.getElementById("submit-allowance");
+const submitExpenses = document.getElementById("submit-expenses");
+const allowanceAmount = document.getElementById("allowance-amount");
+const expensesAmount = document.getElementById("expenses-amount");
+const balanceAmount = document.getElementById("balance-amount");
+const expenseName = document.getElementById("add-expense-name");
+const list = document.getElementById("list");
+let tempAmount = 0;
+
+submitAllowance.addEventListener("click", () => {
+
+  tempAmount = addAllowance.value;
+  
+  if(tempAmount == "" || tempAmount < 0){
+
+  }else{
+    //Set Budget
+    allowanceAmount.innerHTML = tempAmount;
+    //Set Balance
+    balanceAmount.innerHTML = tempAmount - expensesAmount.innerText;
+    //Clear Input Box
+    addAllowance.value = "";
+  }
+});
+
+submitExpenses.addEventListener("click", () => {
+  let  amount = parseFloat(addExpenses.value);
+  let totalAmount = parseFloat(expensesAmount.innerText) +  amount;
+  
+  expensesAmount.innerHTML = totalAmount;
+  balanceAmount.innerHTML = parseFloat(balanceAmount.innerText) - amount;
+  
+  listCreator(expenseName.value, addExpenses.value);
+
+
+
+});
+
+const listCreator = (_expenseName, _expenseValue) => {
+
+  let sublistContent = document.createElement("div");
+  sublistContent.classList.add("sublist-content","flex-space");
+  list.appendChild(sublistContent);
+  sublistContent.innerHTML = `<p class="product">${_expenseName}</p><p class="amount">${_expenseValue}</p>`;
+
+};
+
+
 
 
