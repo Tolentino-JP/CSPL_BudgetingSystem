4f650cc17893db2c57b9493d1325cca2a79505b4
bisenteherradura <vincentaisner12@gmail.com>
2024-05-15 15:17:37 +0800

    updated 05/15/2024

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const {router: login} = require('./public/javascript/login');
@@ -15,8 +16,6 @@ app.get('/', (req, res) => {
 app.use(express.static('public'));
 app.use('/login', login);
 app.use('/addTransaction', addTransaction);
-
-
 
 
 
