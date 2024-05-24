3909667f08aaca060f1d1344c003343591a96c3e
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-05-24 11:37:48 +0800

    UPDATED 05/24/2024 DONE

--- public/javascript/add-transact.js
+++ public/javascript/add-transact.js
@@ -7,30 +7,32 @@ const myObject = require('./login');
 
 router.use(express.static(path.join(__dirname, '../../public')));
 
-function GetDate(){
+// function GetDate(){
 
-    const currentDate = new Date();
+//     const currentDate = new Date();
 
-    // Get month, day, and year
-    const month = currentDate.getMonth() + 1; // January is 0, so we add 1
-    const day = currentDate.getDate();
-    const year = currentDate.getFullYear();
-    // Add leading zeros if needed
-    const formattedMonth = month < 10 ? `0${month}` : month;
-    const formattedDay = day < 10 ? `0${day}` : day;
-    // Construct the formatted date string (mm-dd-yyyy)
-    const formattedDate = `${year}-${formattedMonth}-${formattedDay}`;
-    return formattedDate;
-}
+//     // Get month, day, and year
+//     const month = currentDate.getMonth() + 1; // January is 0, so we add 1
+//     const day = currentDate.getDate();
+//     const year = currentDate.getFullYear();
+//     // Add leading zeros if needed
+//     const formattedMonth = month < 10 ? `0${month}` : month;
+//     const formattedDay = day < 10 ? `0${day}` : day;
+//     // Construct the formatted date string (mm-dd-yyyy)
+//     const formattedDate = `${year}-${formattedMonth}-${formattedDay}`;
+//     return formattedDate;
+// }
 
 router.use(express.static(path.join(__dirname, '../../../public')));
 
 
 router.post('/allowance', (req, res) => {
 
-    const { 'add-allowance': allowance } = req.body;
+    const { 'add-allowance': allowance , 'set-date-allowance': date } = req.body;
 
-    let sqlQuery = `INSERT INTO allowance (user_id, amount, date) VALUES (${myObject.user_id}, '${allowance}', '${GetDate()}')`;
+    console.log(date);
+
+    let sqlQuery = `INSERT INTO allowance (user_id, amount, date) VALUES (${myObject.user_id}, '${allowance}', '${date}')`;
 
     myObject.client.query(sqlQuery, (err, result) => {
         if (err){
@@ -45,9 +47,9 @@ router.post('/allowance', (req, res) => {
 });
 
 router.post('/expenses', (req,res) => {
-    const { 'add-expenses': expenses, 'add-expenses-name': expenseName} = req.body;
+    const { 'add-expenses': expenses, 'add-expenses-name': expenseName, 'set-date-expense' : date} = req.body;
 
-    let sqlQuery = `INSERT INTO expenses (user_id, expense_name, expense_amount, date) VALUES (${myObject.user_id},'${expenseName}',${expenses},'${GetDate()}')`;
+    let sqlQuery = `INSERT INTO expenses (user_id, expense_name, expense_amount, date) VALUES (${myObject.user_id},'${expenseName}',${expenses},'${date}')`;
 
     myObject.client.query(sqlQuery, (err,result) => {
         if (err){
